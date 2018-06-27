6179def436a414aad4dab24aaaf4381d8bc61859
Shiva Prasad <sp@shiv19.com>
2018-06-27 21:56:05 +0530

    demo ready for ios

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,14 +1,28 @@
 import { Observable } from 'tns-core-modules/data/observable';
+import { ObservableArray } from 'tns-core-modules/data/observable-array';
 import { RadImagepicker } from 'nativescript-rad-imagepicker';
 
 export class HelloWorldModel extends Observable {
-  public message: string;
+  public images;
   private radImagepicker: RadImagepicker;
 
   constructor() {
     super();
 
+    this.images = new ObservableArray([]);
     this.radImagepicker = new RadImagepicker();
-    this.message = this.radImagepicker.message;
+  }
+
+  pickImage() {
+    this.radImagepicker.pick({
+      done: (selectedImages) => {
+        this.images.length = 0;
+        for (let i = 0; i < selectedImages.length; i++) {
+          this.images.push({
+            src: selectedImages[i]
+          });
+        }
+      }
+    });
   }
 }
