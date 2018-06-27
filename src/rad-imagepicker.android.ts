2ad5ea2da4e36bcb51b51d3b8b0de50ecec5df29
Shiva Prasad <sp@shiv19.com>
2018-06-28 01:52:51 +0530

    android in the making...

--- src/rad-imagepicker.android.ts
+++ src/rad-imagepicker.android.ts
@@ -1,5 +1,6 @@
 import { Common, PickerOptions } from './rad-imagepicker.common';
 import * as app from "tns-core-modules/application";
+import * as utils from 'tns-core-modules/utils/utils';
 
 declare var com: any;
 
@@ -10,10 +11,28 @@ export class RadImagepicker extends Common {
     }
 
     pick(options: PickerOptions): Promise<Array<any>> {
+
+        com.fxn.pix.Pix.start(app.android.foregroundActivity, 100, 5);
+        
+        app.android.foregroundActivity.onActivityResult = function(
+            requestCode,
+            resultCode,
+            data
+        ) {
+            switch (requestCode) {
+                case (100): {
+                    if (resultCode == android.app.Activity.RESULT_OK) {
+                        const returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS);
+                        console.log(returnValue);
+                    }
+                }
+                break;
+            }
+        }
+
+        // TODO: resolve the promise with image array
         return new Promise((resolve, reject) => {
-            console.log(com.fxn.pix.Pix);
             resolve ([]);
-            // com.fxn.pix.Pix.start(app.android.foregroundActivity, 100, 5);
         })
     }
 }
